6e38df384c8d39d25b5e63521a28d4c2202fcb5a
Adarsh <2017csb1064@iitrpr.ac.in>
2020-05-23 03:02:25 +0530

    Convert Image to base64 String and send using ngrok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -53,7 +56,7 @@ class MyImagePickerState extends State<MyImagePicker>{
 				),
 				textColor: Colors.white,
 				color: Colors.grey,
-				padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+				padding: EdgeInsets.fromLTRB(6, 6, 6, 6),
 			)
 		);
 	}
@@ -68,9 +71,59 @@ class MyImagePickerState extends State<MyImagePicker>{
 				),
 				textColor: Colors.white,
 				color: Colors.grey,
+				padding: EdgeInsets.fromLTRB(6, 6, 6, 6),
+			)
+		);
+	}
+
+	Container buttonGetFoodDetails(File imageURI){
+		if(imageURI==null)
+			return new Container();
+		return Container(
+			margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+			child: RaisedButton(
+				onPressed: () async {
+          String result = await sendImage(imageURI);
+          print('Hey $result');},
+				child: Text(
+					'Get Details'
+				),
+				textColor: Colors.white,
+				color: Colors.black,
 				padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
 			)
 		);
+	}
+
+	String convertImgToBase64(File imageURI){
+		final bytes= imageURI.readAsBytesSync();
+		String imgB64= base64Encode(bytes);
+		return imgB64;
+	}
+	// Future<http.Response> sendImage(File imageURI) async{
+
+	Future<String> sendImage(File imageURI) async{
+		String imgBase64= convertImgToBase64(imageURI);
+		print('$imgBase64');
+    Map foodItem= {
+      'food_item': imgBase64
+    };
+    var body= json.encode(foodItem);
+		http.Response response= await http.post(
+			'http://c471f2f1.ngrok.io/food_item/',   //Using Ngrok Temporarily.
+			headers:{
+			  "Content-Type": "application/json"
+			},
+			body: body
+		);
+		print('Status Code: ${response.statusCode}');
+		if(response.statusCode==201){
+			Map<String, dynamic> decodedJson= jsonDecode(response.body);
+			print(decodedJson);
+			return decodedJson['food_item'];
+		}
+		else
+			throw Exception('Failed');
 	}
 
 	Text uploadImageMessage(){
@@ -89,6 +142,7 @@ class MyImagePickerState extends State<MyImagePicker>{
 					children: <Widget>[
 						imageURI == null?uploadImageMessage():Image.file(imageURI,
 										width: 300, height: 200, fit: BoxFit.cover),
+						buttonGetFoodDetails(imageURI),
 						buttonContainerCamera(),
 						buttonContainerGallary(),
 					],
